429532e9120272e80a491e5ca34cb5d17401109d
karti <kartikeya968@gmail.com>
2025-05-02 16:23:51 +0530

    by kk

--- all/src/com/A.java
+++ all/src/com/A.java
@@ -5,7 +5,11 @@ public class A
 public static void main(String[] args) {
 	System.out.println("welcome");
 }
- {
+    {
 	System.out.println("m1");
-}	
+    }
+    
+    {
+		System.out.println("m2");
+	}	
 }
